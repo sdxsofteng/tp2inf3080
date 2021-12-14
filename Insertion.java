7078bbf91f5ec79f4f2a183aef6869395544bec4
simon.desormeaux <71537333+sdxsofteng@users.noreply.github.com>
2021-12-13 21:02:54 -0500

    Create Insertion.java

---
+++ Insertion.java
@@ -0,0 +1,25 @@
+public class Insertion {
+    public int idCandidat;
+    public int nombresdevotes;
+
+    public Insertion(int idCandidat, int nombresdevotes) {
+        this.idCandidat = idCandidat;
+        this.nombresdevotes = nombresdevotes;
+    }
+
+    public int getIdCandidat() {
+        return idCandidat;
+    }
+
+    public void setIdCandidat(int idCandidat) {
+        this.idCandidat = idCandidat;
+    }
+
+    public int getNombresdevotes() {
+        return nombresdevotes;
+    }
+
+    public void setNombresdevotes(int nombresdevotes) {
+        this.nombresdevotes = nombresdevotes;
+    }
+}
